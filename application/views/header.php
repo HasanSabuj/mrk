d12dfadb751242ff5368d2ae1022ed039fe399bb
M Hasan <greensabuj350@gmail.com>
2018-09-01 00:03:09 +0600

    change the system title

--- application/views/header.php
+++ application/views/header.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>MinMax Tech Ltd.</title>
+    <title>Marketing Team Management System</title>
     
     <!-- Animate.css -->
     <?php
